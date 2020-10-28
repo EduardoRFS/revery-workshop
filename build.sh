908f9daf8a0f7c35662ecb7adf49fbf702da9420
EduardoRFS <theeduardorfs@gmail.com>
2020-10-28 09:23:17 -0300

    add build script for android

--- build.sh
+++ build.sh
@@ -1,28 +1,39 @@
 #! /bin/sh
 
 ## ./build.sh @<toolchain>
-## arm64: ./build.sh ios.arm64
-## simulator: ./build.sh ios.simulator.x86_64
+## ios arm64: ./build.sh ios.arm64
+## ios simulator: ./build.sh ios.simulator.x86_64
+## android arm64: ./build.sh android.arm64
+## android emulator: ./build.sh android.x86_64
 	
 set -e	
 set -u	
 
 TOOLCHAIN="${1:-ios.arm64}"
-SOURCE_BINARY_NAME="8-completed"
+MODULE_NAME="8-completed"
+SHARED_NAME="libmain.so"
 BINARY_NAME="ReveryWorkshop"
 
 if [ ! -d "$TOOLCHAIN.esy.lock" ]; then
   esy
   esy generate $toolchain
 fi
+
 esy @$TOOLCHAIN
-	
-INSTALL_FOLDER=$(esy @$TOOLCHAIN sh -c 'echo $cur__target_dir/install/default.$ESY_TOOLCHAIN')
+TARGET_FOLDER=$(esy @$TOOLCHAIN sh -c 'echo $cur__target_dir/default.$ESY_TOOLCHAIN')
 	
 # uses a temporary folder to avoid
 
-cd Xcode
-cp $INSTALL_FOLDER/bin/* .
-cp $SOURCE_BINARY_NAME $BINARY_NAME
+if [ "$TOOLCHAIN" == "android.arm64" ] || [ "$TOOLCHAIN" == "android.x86_64" ]; then
+  ARCH="arm64-v8a"
+  if [ "$TOOLCHAIN" == "android.x86_64" ]; then
+    ARCH="x86_64"
+  fi
 
-cd ..
+  esy @$TOOLCHAIN dune build -x $TOOLCHAIN --profile=android 8-completed.so
+  cp "$TARGET_FOLDER/${MODULE_NAME}.so" Android/app/libs/$ARCH/$SHARED_NAME
+fi
+if [ "$TOOLCHAIN" == "ios.arm64" ] || [ "$TOOLCHAIN" == "ios.simulator.x86_64" ]; then
+  cp "$TARGET_FOLDER/${MODULE_NAME}.exe" Xcode/ReveryWorkshop
+fi
+
